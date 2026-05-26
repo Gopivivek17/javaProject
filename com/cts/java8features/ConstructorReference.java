80bf1d88e2a0b5191c42384006fd4b1dd1893ae5
Gopivivek17 <Shiramshetti.GopiVivek@cognizant.com>
2026-05-26 12:28:17 +0530

    Updated

--- com/cts/java8features/ConstructorReference.java
+++ com/cts/java8features/ConstructorReference.java
@@ -12,6 +12,11 @@ class Student1{
 		this.age = age;
 		this.marks = marks;
 	}
+	
+	Student1(){
+		System.out.println("Constructor overloading");
+		
+	}
 
 	@Override
 	public String toString() {
@@ -30,7 +35,9 @@ interface Interf{
 public class ConstructorReference {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// 1. Using Lambda Expression:
+        // Manually passing arguments to the 'new' keyword
 		
 		Interf i = (name, rollNo, age, marks) -> new Student1(name, rollNo, age, marks);
 		
@@ -38,8 +45,16 @@ public class ConstructorReference {
 		
 		System.out.println(s1);
 		
-		Interf r = Student1::new;
+		// 2. Using Constructor Reference (ClassName::new):
+        // The compiler maps Interf.get() directly to the Student1 constructor.
+        // This is cleaner and more readable than the Lambda version above.
 		
+		// Here get method in the Interface Interf refers to the constructor in Student1 class this is called constructor reference
+		
+		
+		Interf r = Student1::new; 
+		
+		// and also the get method only refers to the Student1 constructor of same arguments 
 		Student1 s2 = r.get("Ram",25078,20,89);
 		
 		System.out.println(s2);
